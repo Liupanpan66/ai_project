5ba9ef3a4dea094cd0d1d4cc909e55eb36745243
lpp <3448874507@qq.com>
2026-01-19 15:03:04 +0800

    测试远程

---
+++ text/main.py
@@ -0,0 +1,55 @@
+# from fastapi import FastAPI
+# app = FastAPI()
+# @app.get("/")
+# def read_root():
+#     return {"Hello":"World"}
+
+# from pydantic import BaseModel
+# from fastapi import FastAPI
+# app = FastAPI()
+# class Item(BaseModel):
+#     name: str
+#     description: str = None
+#     price: float
+#     tax: float = None
+# @app.post("/items")
+# async def create_items(item: Item):
+#     return item
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
